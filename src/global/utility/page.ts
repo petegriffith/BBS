f168cf4ae2f502082cfc4c1fa76d59b8dd56e89c
Peter <griffitp12@gmail.com>
2021-11-24 16:28:25 +0900

    url test 3

--- src/global/utility/page.ts
+++ src/global/utility/page.ts
@@ -2,6 +2,9 @@ export const checkURL = () => {
   let communityId: number, langFromURL: string | undefined
   const siteURLString = document.location.pathname
   console.log("URL:", siteURLString)
+  console.log("2:", document.location)
+  console.log("3", document)
+  console.log("4", window)
   // this first if check and it's else block are for dev only and need to be deleted once the app is hosted
   if (siteURLString.length > 1) {
     const codedURL = new URL(siteURLString)
