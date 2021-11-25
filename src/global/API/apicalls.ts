6f827da7da0dd0918a6984390d010ba91dedef60
Peter <griffitp12@gmail.com>
2021-11-25 12:32:34 +0900

    env test 5

--- src/global/API/apicalls.ts
+++ src/global/API/apicalls.ts
@@ -8,7 +8,7 @@ const instance = axios.create({
   timeout: 10000,
 })
 
-console.log('env var:', process.env.VITE_API_URL)
+console.log('env var:', import.meta.env.VITE_API_URL)
 
 const responseBody = (response: AxiosResponse<any>) => response.data
 
@@ -19,24 +19,24 @@ const requests = {
 }
 
 export const community = {
-  getProfileById: (communityId: number, lang: string): Promise<communityProfile> => requests.get(`${process.env.VITE_API_URL}get/profile/${communityId}?lang_cd=${lang}`),
-  getMessagesById: (communityId: number, lang: string): Promise<communityMessageContainer> => requests.get(` ${process.env.VITE_API_URL}get/${communityId}?lang_cd=${lang}&page=0`),
+  getProfileById: (communityId: number, lang: string): Promise<communityProfile> => requests.get(`${import.meta.env.VITE_API_URL}get/profile/${communityId}?lang_cd=${lang}`),
+  getMessagesById: (communityId: number, lang: string): Promise<communityMessageContainer> => requests.get(` ${import.meta.env.VITE_API_URL}get/${communityId}?lang_cd=${lang}&page=0`),
 }
 
 export const user = {
-  getAnonymousUserToken: (userProfile: anonymousUserProfile): Promise<userTokenObject> => requests.post(` ${process.env.VITE_API_URL}user/anonymous/create`, userProfile),
-  userJoinCommunity: (communityId: number, accessToken: string): Promise<unknown> => requests.post(` ${process.env.VITE_API_URL}user/join/${communityId}`, {}, accessToken),
+  getAnonymousUserToken: (userProfile: anonymousUserProfile): Promise<userTokenObject> => requests.post(` ${import.meta.env.VITE_API_URL}user/anonymous/create`, userProfile),
+  userJoinCommunity: (communityId: number, accessToken: string): Promise<unknown> => requests.post(` ${import.meta.env.VITE_API_URL}user/join/${communityId}`, {}, accessToken),
 }
 
 export const post = {
-  postMessageToBBS: (messageBody: ajaxPostObject, accessToken: string): Promise<ajaxPostObject> => requests.post(` ${process.env.VITE_API_URL}post`, messageBody, accessToken),
-  postImageToBBS: (messageBody: ajaxPostObject, accessToken: string): Promise<ajaxPostObject> => requests.post(` ${process.env.VITE_API_URL}post/image`, messageBody, accessToken),
-  replyToBBSMessage: (replyMessageBody: ajaxReplyObject, accessToken: string): Promise<ajaxPostObject> => requests.post(` ${process.env.VITE_API_URL}reply`, replyMessageBody, accessToken),
+  postMessageToBBS: (messageBody: ajaxPostObject, accessToken: string): Promise<ajaxPostObject> => requests.post(` ${import.meta.env.VITE_API_URL}post`, messageBody, accessToken),
+  postImageToBBS: (messageBody: ajaxPostObject, accessToken: string): Promise<ajaxPostObject> => requests.post(` ${import.meta.env.VITE_API_URL}post/image`, messageBody, accessToken),
+  replyToBBSMessage: (replyMessageBody: ajaxReplyObject, accessToken: string): Promise<ajaxPostObject> => requests.post(` ${import.meta.env.VITE_API_URL}reply`, replyMessageBody, accessToken),
   replyWithImageToBBSMessage: (replyMessageBody: ajaxReplyObject, accessToken: string): Promise<ajaxPostObject> =>
-    requests.post(` ${process.env.VITE_API_URL}reply/image`, replyMessageBody, accessToken),
+    requests.post(` ${import.meta.env.VITE_API_URL}reply/image`, replyMessageBody, accessToken),
 }
 
 export const cloud = {
-  getCloudinaryToken: (accessToken: string): Promise<ajaxSignatureResponseObject> => requests.get(` ${process.env.VITE_API_URL}image/upload`, accessToken),
+  getCloudinaryToken: (accessToken: string): Promise<ajaxSignatureResponseObject> => requests.get(` ${import.meta.env.VITE_API_URL}image/upload`, accessToken),
   uploadImageToCloudinary: (url: string, cloudinaryData: unknown, options: AxiosRequestConfig<any>): Promise<cloudinaryUploadApiResponse> => requests.postCustom(`${url}`, cloudinaryData, options),
 }
