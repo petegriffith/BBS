d098544b66ebead8b5716eed3573a755e6b4b465
Peter <griffitp12@gmail.com>
2021-11-25 13:53:18 +0900

    test 10

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
 
-const viteEnv = {}
-Object.keys(process.env).forEach((key) => {
-  if (key.startsWith(`VITE_`)) {
-    viteEnv[`import.meta.env.${key}`] = process.env[key]
-  }
-})
-
 // https://vitejs.dev/config/
 export default defineConfig({
- /*  alias: {
-    '@': require('path').resolve(__dirname, 'src')
-  }, */
-  define: viteEnv,
   plugins: [
     vue(),
     vueI18n({
